ffccf0dfab85c7bef68aa0140c717f2dc0923d63
jshin313 <jacobshin313@gmail.com>
2019-08-13 02:51:43 +0000

    Code for loading favicon

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var app = express();
 
 app.use(express.static('public'));
 
+//app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
+
+
 //make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
