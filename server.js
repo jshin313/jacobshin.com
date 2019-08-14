9f903a61cca648781b14c595e977479601dc2262
jshin313 <jacobshin313@gmail.com>
2019-08-14 22:22:52 +0000

    HTTPS WORKSgit add server.js

--- server.js
+++ server.js
@@ -1,16 +1,22 @@
+var fs = require("fs");
 var express = require("express");
-
+var https = require("https")
 
 var app = express();
 
+// HTTPS Stuff
+var server = https.createServer({
+	  key: fs.readFileSync('/etc/letsencrypt/live/jacobshin.com/privkey.pem'),
+	  cert: fs.readFileSync('/etc/letsencrypt/live/jacobshin.com/cert.pem'),
+	  ca: fs.readFileSync('/etc/letsencrypt/live/jacobshin.com/chain.pem')
+}, app).listen(443, () => {
+	  console.log('Listening...')
+})
+
 app.use(express.static('public'));
 
-//make way for some custom css, js and images
+// make way for some custom css, js and images
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use('/js', express.static(__dirname + '/public/js'));
 app.use('/images', express.static(__dirname + '/public/images'));
 
-var server = app.listen(8080, function(){
-    var port = server.address().port;
-    console.log("Server started at http://localhost:%s", port);
-});
